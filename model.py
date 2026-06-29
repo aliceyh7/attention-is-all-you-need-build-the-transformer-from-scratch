ffca4dfd420a062d5afe1aeb44d1057cf6a55d0a
Alice <alicehu2004@gmail.com>
2026-06-29 03:29:16 -0700

    Step 2: build_id_to_token_vocab

--- model.py
+++ model.py
@@ -22,8 +22,14 @@ def build_token_to_id_vocab(sentences, specials=('<pad>', '<bos>', '<eos>', '<un
                 dict[token] = len(dict)
     return dict
 
-# Step 2 - build_id_to_token_vocab (not yet solved)
-# TODO: implement
+# Step 2 - build_id_to_token_vocab
+def build_id_to_token_vocab(token_to_id):
+    
+    dict = {}
+    for token, token_id in token_to_id.items():
+        dict[token_id] = token
+    
+    return dict
 
 # Step 3 - encode_sentence_to_ids (not yet solved)
 # TODO: implement
