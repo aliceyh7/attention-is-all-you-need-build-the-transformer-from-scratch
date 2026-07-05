688ef3dbb9c22cb5d17103a9a60a0158e8c9d9e2
Alice <alicehu2004@gmail.com>
2026-07-05 15:07:55 -0700

    Step 74: pick_next_token_by_argmax

--- model.py
+++ model.py
@@ -1228,8 +1228,18 @@ def run_training_loop_for_steps(batches, parameter_list, model_params, optimizer
         
     return losses
 
-# Step 74 - pick_next_token_by_argmax (not yet solved)
-# TODO: implement
+# Step 74 - pick_next_token_by_argmax
+import torch
+
+def pick_next_token_by_argmax(final_step_logits):
+    """Greedy: return argmax token id per batch row.
+
+    final_step_logits: FloatTensor of shape (batch, vocab_size)
+    returns: LongTensor of shape (batch,)
+    """
+    # TODO: pick the next greedy token id by taking the argmax over the vocab axis
+    
+    return torch.argmax(final_step_logits, dim=-1)
 
 # Step 75 - compute_length_penalty (not yet solved)
 # TODO: implement
